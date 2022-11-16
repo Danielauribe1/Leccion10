75068f9fd22add04ed3ac383d576a51176816778
DUM <114031187+Danielauribe1@users.noreply.github.com>
2022-11-16 12:32:43 -0500

    Segundo avance tercer semana de noviembre JS

---
+++ 07-02SiVariableEsNum.js
@@ -0,0 +1,26 @@
+//Si la variable es un numero o no
+
+let miNumero = "17";
+
+let edad = Number(miNumero); 
+console.log( edad );
+
+if( isNaN(edad)){
+    console.log("No es un número");
+}
+else{
+    if(edad >= 18){
+        console.log("Puede votar");
+    }
+    else{
+        console.log("Muy joven para votar");
+    }    
+}
+
+if( isNaN(edad)){
+    console.log("No es un número");
+}
+else{
+    let resultado = (edad >= 18)? "Puede votar" : "Muy joven para votar";
+    console.log( resultado ); 
+}
