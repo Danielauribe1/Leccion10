922859aefe42c2fd33070afab6bebf26eb7a0f05
DUM <114031187+Danielauribe1@users.noreply.github.com>
2022-11-18 13:58:18 -0500

    Cuarto avance tercer semana de noviembre JS

---
+++ 07-04SentenciaIfElse.js
@@ -0,0 +1,10 @@
+//Sentencia if-else
+let condicion = false;
+
+if(condicion){
+    console.log("Condición verdadera");
+    console.log("fin del programa");
+}
+else{
+    console.log("Condición falsa");
+}
